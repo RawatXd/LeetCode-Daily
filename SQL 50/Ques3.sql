f864493bd87a04ddba0dc7059c711967d0d60a0c
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-02-12 23:54:50 +0530

    Create Ques3.sql

---
+++ SQL 50/Ques3.sql
@@ -0,0 +1,6 @@
+--LeetCode Question 3
+--Write an SQL query to find the name, population and area of the countries in the World with a population greater than 25 million.
+
+SELECT name , population , area 
+FROM World
+Where population > 25000000 ;
