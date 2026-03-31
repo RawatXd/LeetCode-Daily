e4ca54804e8486cf68492d143ffff715f3d150f2
Abhishek Rawat <rawatabhishek.123321@gmail.com>
2026-03-31 22:01:18 +0530

    Update product_price_at_given_date.sql

--- SQL 50/Advanced Select and Joins/product_price_at_given_date.sql
+++ SQL 50/Advanced Select and Joins/product_price_at_given_date.sql
@@ -1,1 +1,22 @@
 -- Product Price at Given Date
+
+-- Rule 1: Get the latest price for products that changed on or before 2019-08-16
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+UNION
+
+-- Rule 2: Get products that had NO changes before or on 2019-08-16
+SELECT DISTINCT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+);
